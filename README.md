c618d6092d003eeb79f5baf64411bfd11b14194a
Zayd Antony <zaydantony123@gmail.com>
2026-01-16 07:13:28 +0300

    Initial project setup and models setup for accounts app

---
+++ README.md
@@ -0,0 +1,21 @@
+# Kura API 🇰🇪
+
+A secure online polling backend built with Django REST Framework.
+
+## Tech Stack
+- Django + DRF
+- MySQL
+- Redis + Celery
+- Docker
+- GitHub Actions
+
+## Git Flow
+- main → production
+- develop → staging
+- feature/* → development 
+
+## Apps Include
+- accounts - for user registration and login
+- polls - for admins creating polls
+- Votes - For the voting process
+- Analysis - Thinking of doing analysis with the help of ai or celery(work to be confirmed)
---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        VOTER = "VOTER", "Voter"
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.VOTER
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"

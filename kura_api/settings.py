da8a7a1a76dde169a4a7b7419bca8966413be5ea
Zayd Antony <zaydantony123@gmail.com>
2026-01-16 09:42:53 +0300

    Successful local mysql setup

--- kura_api/settings.py
+++ kura_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'kura_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kura_api',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD':'Antony123$#',
+        'PORT': '3306',
     }
 }
 

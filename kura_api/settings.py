4fa3079a57f804209dbd62a5e763de270ae986ab
Zayd Antony <zaydantony123@gmail.com>
2026-01-18 07:32:22 +0300

    Created the models for my polls app

--- kura_api/settings.py
+++ kura_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'rest_framework',
+    'polls',
 ]
 
 AUTH_USER_MODEL = "accounts.User"
---
+++ polls/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+User = settings.AUTH_USER_MODEL
+
+
+# Create your models here.
+class Poll(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    createdby = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="created_polls"
+    )
+
+    is_active = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Option(models.Model):
+    poll = models.ForeignKey(
+        Poll,
+        on_delete=models.CASCADE,
+        related_name="options"
+    )
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.text} ({self.poll.title})"

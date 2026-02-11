8141278d862886f030393fab91b29b2290af1eec
Zayd Antony <zaydantony123@gmail.com>
2026-02-11 10:36:23 +0300

    syntax fix

--- kura_api/settings.py
+++ kura_api/settings.py
@@ -103,7 +103,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE/DIR'db.sqlite3',
+        'NAME': BASE_DIR/ 'db.sqlite3',
         
 
     }
